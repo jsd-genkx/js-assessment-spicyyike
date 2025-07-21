3bbece4ae8015a9045613d29c86786dbe027936d
Rosarin <zenpinn@gmail.com>
2025-07-21 14:14:17 +0700

    delete export + add format

--- main.js
+++ main.js
@@ -5,60 +5,56 @@ import clear from "clear-screen";
 
 const prompt = promptSync({ sigint: true });
 
-
-export const hat = "^";
-export const hole = "O";
-export const fieldCharacter = "░";
-export const pathCharacter = "*";
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
 
 class Field {
+  constructor(field = [[]]) {
+    this.field = field;
 
-	constructor(field = [[]]) {
-		this.field = field;
+    // Replace with your own code //
+    this.positionRow = null;
+    this.positionCol = null;
+    this.gameOver = false;
+  }
 
-		// Replace with your own code //
-		this.positionRow = null;
-		this.positionCol = null;
-		this.gameOver = false
-	}
+  static createField(holesPercent, row, column) {
+    //create a field
+    const holes = Math.ceil((holesPercent / 100) * row * column);
+    const field = [];
+    for (let i = 0; i < row; i++) {
+      const r = [];
+      for (let j = 0; j < column; j++) {
+        r.push(fieldCharacter);
+      }
+      field.push(r);
+    }
 
-	static createField(holesPercent,row,column) { 
-	//create a field
-	const holes = Math.ceil(holesPercent/100 * row * column);
-	const field = [];
-	for (let i=0; i < row; i++) {
-	const r = []
-	for (let j=0; j < column; j++) {
-		r.push(fieldCharacter)
-	}
-	field.push(r)
-}
+    // place holes randomly
+    let placeHoles = holes;
+    while (placeHoles > 0) {
+      const rowRandom = Math.floor(Math.random() * field.length);
+      const columnRandom = Math.floor(Math.random() * field[0].length);
+      if (field[rowRandom][columnRandom] === fieldCharacter) {
+        field[rowRandom][columnRandom] = hole;
+        placeHoles--;
+      }
+    }
 
+    //place hat randomly
+    let placeHat = false;
+    while (!placeHat) {
+      const rowRandom = Math.floor(Math.random() * field.length);
+      const columnRandom = Math.floor(Math.random() * field[0].length);
+      if (field[rowRandom][columnRandom] === fieldCharacter) {
+        field[rowRandom][columnRandom] = hat;
+        placeHat = true;
+      }
+    }
 
-	// place holes randomly
-	let placeHoles = holes
-	while (placeHoles > 0) {
-		const rowRandom = Math.floor(Math.random() *field.length);
-		const columnRandom = Math.floor(Math.random() *field[0].length);
-		if (field[rowRandom][columnRandom] === fieldCharacter) {
-			field[rowRandom][columnRandom] = hole;
-			placeHoles--;
-		}
-	}
-
-	//place hat randomly
-	let placeHat = false;
-	while (!placeHat) {
-		const rowRandom = Math.floor(Math.random() *field.length);
-		const columnRandom = Math.floor(Math.random() *field[0].length);
-		if (field[rowRandom][columnRandom] === fieldCharacter) {
-			field[rowRandom][columnRandom] = hat;
-			placeHat = true;
-		}
-		
-	} 
-
-	/*		let placeActor = false;
+    /*		let placeActor = false;
 	while (!placeActor) {
 		const rowRandom = Math.floor(Math.random() *field.length);
 		const columnRandom = Math.floor(Math.random() *field[0].length);
@@ -71,134 +67,126 @@ class Field {
 
 	} */
 
+    //console.log(field)
+    return field;
+  }
 
-//console.log(field)
-return field
-}
+  // Print field //
+  print() {
+    clear();
+    for (let row of this.field) {
+      console.log(row.join(" "));
+    }
+  }
 
-	// Print field //
-	print() {
-		clear();
-		for (let row of this.field) {
-			console.log(row.join(" ")) 
-		}
+  move(direction) {
+    //console.log(`Moving ${this.positionRow}, ${this.positionCol} to ${direction}`);
+    if (direction === "l") {
+      this.moveLeft();
+    } else if (direction === "r") {
+      this.moveRight();
+    } else if (direction === "u") {
+      this.moveUp();
+    } else if (direction === "d") {
+      this.moveDown();
+    }
+  }
 
-	}
-	
+  moveLeft() {
+    this.positionCol--;
+  }
 
-	move(direction) {
-		//console.log(`Moving ${this.positionRow}, ${this.positionCol} to ${direction}`);
-	if (direction === "l") {
-		this.moveLeft()
-	}
-	else if (direction === "r") {
-		this.moveRight()
-	}
-	else if (direction === "u") {
-		this.moveUp()
-	}
-	else if (direction === "d") {
-		this.moveDown()
-	}
-}
+  moveRight() {
+    this.positionCol++;
+  }
 
+  moveUp() {
+    this.positionRow = this.positionRow - 1;
+  }
 
-	moveLeft() {
-		this.positionCol--
-		
-		
-	}
+  moveDown() {
+    this.positionRow = this.positionRow + 1;
+  }
 
-	moveRight() {
-		this.positionCol++
-		
-	}
+  checkCondition() {
+    //console.log(`positionRow: ${positionRow}, positionCol: ${positionCol}`)
+    if (
+      this.field.length <= this.positionRow ||
+      this.field[0].length <= this.positionCol ||
+      this.positionRow < 0 ||
+      this.positionCol < 0
+    ) {
+      this.gameOver = true;
+      console.log("Loses by attempting to move “outside” the field.");
+      return;
+    } else if (this.field[this.positionRow][this.positionCol] === "O") {
+      this.gameOver = true;
+      console.log("Loses by landing on (and falling in) a hole.");
+      return;
+    } else if (this.field[this.positionRow][this.positionCol] === "^") {
+      this.gameOver = true;
+      console.log("Wins by finding their hat.");
+      return;
+    }
+  }
 
-	moveUp() {
-		this.positionRow = this.positionRow - 1
-	}
+  update() {
+    if (!this.gameOver) {
+      this.field[this.positionRow][this.positionCol] = pathCharacter;
+    }
+  }
 
-	moveDown() {
-		this.positionRow = this.positionRow + 1
-		
-	}
+  createActor() {
+    let placeActor = false;
+    while (!placeActor) {
+      const rowRandom = Math.floor(Math.random() * this.field.length);
+      const columnRandom = Math.floor(Math.random() * this.field[0].length);
+      if (this.field[rowRandom][columnRandom] === fieldCharacter) {
+        this.field[rowRandom][columnRandom] = pathCharacter;
+        this.positionRow = rowRandom;
+        this.positionCol = columnRandom;
+        placeActor = true;
+      }
+    }
+  }
 
-	checkCondition() {
-		//console.log(`positionRow: ${positionRow}, positionCol: ${positionCol}`)
-	if (this.field.length <= this.positionRow || this.field[0].length <= this.positionCol || this.positionRow < 0 || this.positionCol < 0) {
-		this.gameOver = true
-		console.log('Loses by attempting to move “outside” the field.')
-		return;
-	}
+  checkEdge() {
+    if (
+      this.positionRow === 0 ||
+      this.positionCol === 0 ||
+      this.positionRow === this.field.length - 1 ||
+      this.positionCol === this.field[0].length - 1
+    ) {
+      console.log(
+        "Warning: You are at the edge of the field. Please be careful!"
+      );
+    }
+  }
 
-	else if (this.field[this.positionRow][this.positionCol] === "O") {
-		this.gameOver = true
-		console.log("Loses by landing on (and falling in) a hole.")
-		return;
-	}
-
-	else if (this.field[this.positionRow][this.positionCol] === "^") {
-		this.gameOver = true
-		console.log('Wins by finding their hat.')
-		return;
-		
-}
-
-}
-
-	update() {
-		if (!this.gameOver) {
-		this.field[this.positionRow][this.positionCol] = pathCharacter
-		}
-	}
-
-	createActor() {
-		let placeActor = false;
-	while (!placeActor) {
-		const rowRandom = Math.floor(Math.random() *this.field.length);
-		const columnRandom = Math.floor(Math.random() *this.field[0].length);
-		if (this.field[rowRandom][columnRandom] === fieldCharacter) {
-			this.field[rowRandom][columnRandom] = pathCharacter;
-			this.positionRow = rowRandom;
-			this.positionCol = columnRandom;
-			placeActor = true;
-		}
-
-	}
-	}
-
-	checkEdge() {
-		if (this.positionRow === 0 || this.positionCol === 0 || this.positionRow === this.field.length - 1 || this.positionCol === this.field[0].length - 1) {
-			console.log("Warning: You are at the edge of the field. Please be careful!");
-		}
-	}
-
-
-	runner() {
-		this.createActor()
-		while (!this.gameOver) {
-			this.print()
-			this.checkEdge()
-			const way = prompt("Which way?")
-			this.move(way)
-			this.checkCondition()
-			this.update()
-		}
-	}
-
+  runner() {
+    this.createActor();
+    while (!this.gameOver) {
+      this.print();
+      this.checkEdge();
+      const way = prompt("Which way?");
+      this.move(way);
+      this.checkCondition();
+      this.update();
+    }
+  }
 }
 
 // Game Mode ON
-const newGame = new Field(Field.createField(20,4,4));
+const newGame = new Field(Field.createField(20, 4, 4));
 
-newGame.runner()
+newGame.runner();
 
 // let holeCountWrong = 0;
 // let actorCountWrong = 0;
 // let hatCountWrong = 0;
 //  for (let i = 0; i < 10000; i++) {
 // 	const field = Field.createField(20,3,3)
-// 	//console.log(field) 
+// 	//console.log(field)
 // 	let holeCount = 0;
 // 	let actorCount = 0;
 // 	let hatCount = 0;
@@ -227,5 +215,5 @@ newGame.runner()
 
 // }
 // 	console.log(`Hole count wrong: ${holeCountWrong}`);
-// 	console.log(`Actor count wrong: ${actorCountWrong}`);	
+// 	console.log(`Actor count wrong: ${actorCountWrong}`);
 // 	console.log(`Hat count wrong: ${hatCountWrong}`);
